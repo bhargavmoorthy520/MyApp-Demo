3a3d7fa52cc95ab8772ebce5d728d9d4eebba889
bhargavmoorthy520 <bhargavmoorthycse@gmail.com>
2024-06-02 11:15:52 +0530

    First Commit

---
+++ mysociety/src/main/java/com/example/mysociety/MysocietyApplication.java
@@ -0,0 +1,23 @@
+package com.example.mysociety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MysocietyApplication {
+	
+	@RequestMapping("/")
+	public String Hello() {
+		
+		return "Welcome MySociety";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysocietyApplication.class, args);
+	}
+
+}
